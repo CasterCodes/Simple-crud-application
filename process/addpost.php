62b6bb4b6438e7fa1783df18fdb7a7f420dbd71d
Kevin Caster <56622620+CasterCodes@users.noreply.github.com>
2021-03-14 08:20:21 -0700

    Update addpost.php

--- process/addpost.php
+++ process/addpost.php
@@ -1,9 +1,11 @@
 <?php
 include_once('../config/database.php');
 include_once('../functions.php');
+
 if(filter_has_var(INPUT_POST,'addpost') && $_SERVER['REQUEST_METHOD'] = 'POST'){
-         addPost($connection) ;
+         addPost() ;
 }
+
 if(filter_has_var(INPUT_POST,'updatepost') && $_SERVER['REQUEST_METHOD'] = 'POST'){
-    updatePost($connection);
+    updatePost();
 }
